3eb41219bcfa566f35c8c32635a274403ffa283c
Ollah16 <olaoluwaboluwatife@yahoo.com>
2024-04-29 18:35:33 -0400

    overflow

--- src/app/(chatcomp)/xteristics.js
+++ src/app/(chatcomp)/xteristics.js
@@ -1,4 +1,3 @@
-import Image from "next/image"
 import styles from './chat.module.css'
 import { useAppStore } from "../appcontext"
 
@@ -7,7 +6,7 @@ const XteristicsComp = ({ title, value }) => {
     const { historyToggle } = useAppStore()
 
     return (
-        <ul className={`relative before:inline-block before:h-1 h-max ${styles.functionality_gradient} before:content-[''] before:bg-[#3498dbde] flex flex-col ${historyToggle ? 'md:w-auto lg:w-[33.3%]' : 'lg:w-[33.3%] md:w-auto'} text-[#36454F] pb-2`}>
+        <ul className={`relative overflow-y-auto before:inline-block before:h-1 h-max ${styles.functionality_gradient} before:content-[''] before:bg-[#3498dbde] flex flex-col ${historyToggle ? 'md:w-auto lg:w-[33.3%]' : 'lg:w-[33.3%] md:w-auto'} text-[#36454F] pb-2`}>
             <li className="mx-auto font-bold md:text-lg text-md py-2">{title.charAt(0).toUpperCase() + title.slice(1)}</li>
             {value.map((func, index) => (
 
